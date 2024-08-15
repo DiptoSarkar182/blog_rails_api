78665db5d8197e27f101981f05861df8d3234681
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-08-15 21:51:15 +0600

    added mail service in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,13 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
